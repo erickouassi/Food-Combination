a037c04ee943488d06ed19c904c005090db4e46e
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2024-10-26 17:56:48 -0400

    Create PlantProtein.js

---
+++ PlantProtein.js
@@ -0,0 +1,7 @@
+const PlantProtein = {
+    "tofu": "Plant Protein",
+   "tempeh": "Plant Protein",
+    "hummus": "Plant Protein",
+    "seitan": "Plant Protein",
+    "lentils": "Plant Protein"
+}
