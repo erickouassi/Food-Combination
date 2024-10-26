6e267bb7d0c7ce4241b6b4ed2d8f52f628534c2d
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2024-10-26 18:10:55 -0400

    Create HighAcidFruits.js

---
+++ HighAcidFruits.js
@@ -0,0 +1,10 @@
+const HighAcidFruits = {
+    "strawberries": "High Acid Fruits",
+    "blueberries": "High Acid Fruits",
+    "raspberries": "High Acid Fruits",
+    "pomegranate": "High Acid Fruits",
+    "cantaloupe": "Low Acid Fruits",
+    "blackberries": "High Acid Fruits",
+    "cranberries": "High Acid Fruits",
+    "passion fruit": "High Acid Fruits"
+}
