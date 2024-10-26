299827d9143ebceadc5fa694db1c8744712d4934
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2024-10-26 18:15:26 -0400

    Create LowAcidFruits.js

---
+++ LowAcidFruits.js
@@ -0,0 +1,7 @@
+const LowAcidFruits = {
+    "peach": "Low Acid Fruits",
+    "plum": "Low Acid Fruits",
+    "cherries": "Low Acid Fruits",
+    "apricot": "Low Acid Fruits",
+    "honeydew": "Low Acid Fruits"
+}
