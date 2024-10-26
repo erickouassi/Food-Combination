a34aaffa619b314d8e231abe4edec0a8e21601b0
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2024-10-26 17:59:22 -0400

    Create Vegetables.js

---
+++ Vegetables.js
@@ -0,0 +1,8 @@
+const Vegetables = {
+    "green beans": "Vegetables",
+    "brussels sprouts": "Vegetables",
+    "eggplant": "Vegetables",
+    "mushrooms": "Vegetables",
+    "cauliflower": "Vegetables",
+    "artichoke": "Vegetables"
+}
